8a15ea9a52a8043b12bf77f18688f77ccb298156
vloneivy <cata.serey27@gmail.com>
2023-05-06 23:27:02 -0400

    api y menu
    actualización control 2, avance.

--- gameshop.final/js/script.js
+++ gameshop.final/js/script.js
@@ -17,3 +17,13 @@
       }, false)
     })
   })();
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        document.getElementById('menu-superior').outerHTML
+        =this.responseText;
+    }
+}
+xhttp.open('GET','menu.html', true);
+xhttp.send();
